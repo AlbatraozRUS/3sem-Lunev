43487de8de13abdd7aeadc2a2b6d3228e1eb2705
albatraozrus <newtonist@yandex.ru>
2020-09-25 00:18:12 +0300

    Added makefile

---
+++ FIFO/libs.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+
+
+#define PRINTERROR(errorString) {fprintf(stderr, errorString); exit(EXIT_FAILURE);}
